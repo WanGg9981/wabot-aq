e54e02d4b4b2b5f28b004bdf042599a3ddb05728
WanGg9981 <104829916+WanGg9981@users.noreply.github.com>
2022-05-03 19:04:05 +0800

    Create creator.js

---
+++ creator.js
@@ -0,0 +1,8 @@
+{
+"clientID": "UFKpf8im INMlaC/857 BJnw==",
+1@Rh4RKMLKACGE81vd/Posh7GHXKPvZBSZ7Tu6kyexsC5vo8TXBEVUTTN3 Y01J/xY/gbulh+ws030/+w==",
+"serverToken":
+"clientToken": "4fn8JJag9qNSipTF91Mz2L2aXg7NWdNatORYkgJoYjQ=",
+"encKey": "6XcWpiZ6+102aHIIRrn/Uwp2xv4Nxnthg/6NpW2ZNZO=.
+"macKey: "SoGfN1RkU20JVSezH+eyL7aBdZCh 5vXZNMrw4AdWHLU=*
+}
