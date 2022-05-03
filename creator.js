0be446653860164800438e2ac5859c0bbf5b19e7
WanGg9981 <104829916+WanGg9981@users.noreply.github.com>
2022-05-03 19:10:19 +0800

    Update creator.js

--- creator.js
+++ creator.js
@@ -1,8 +1,7 @@
 {
-"clientID": "UFKpf8im INMlaC/857 BJnw==",
-1@Rh4RKMLKACGE81vd/Posh7GHXKPvZBSZ7Tu6kyexsC5vo8TXBEVUTTN3 Y01J/xY/gbulh+ws030/+w==",
-"serverToken":
-"clientToken": "4fn8JJag9qNSipTF91Mz2L2aXg7NWdNatORYkgJoYjQ=",
-"encKey": "6XcWpiZ6+102aHIIRrn/Uwp2xv4Nxnthg/6NpW2ZNZO=.
-"macKey: "SoGfN1RkU20JVSezH+eyL7aBdZCh 5vXZNMrw4AdWHLU=*
+        "clientID": "UFKpf8imINMlaC/857BJnw==",
+        "serverToken": "1@Rh4RkMLKACGE8lvd/Posh7GHXKPvZBSZ7Tu6kyexsC5vo8TXBEVUTTN3Y0lJ/xY/gbu1h+wsO30/+w==",
+        "clientToken": "4fn8JJag9qNSipTF91Mz2L2aXg7NWdNatORYkgJoYjQ=",
+        "encKey": "6XcWpiZ6+lO2aHIIRrn/Uwp2xv4Nxnthg/6NpW2ZNZ0=",
+        "macKey": "SoGfN1RkU2OJVSezH+eyL7aBdZCh5vXZNMrw4AdWHLU="
 }
